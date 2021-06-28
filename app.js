4d47ab6aad412306eb4561a921cb0e2cd441b4f4
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-28 16:21:47 -0500

    variables user names

---
+++ app.js
@@ -0,0 +1,28 @@
+const profileDataArgs = process.argv.slice(2);
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+const generatePage = (userName, githubName) => { `Name: ${userName}, Github: ${githubName}`;
+    return `
+      Name: ${userName}
+      GitHub: ${githubName}
+    `;
+  }; 
+
+generatePage();
+
+console.log(generatePage());
+
+
+
